29dcc23e3951b54a3a67ffb20cc36cda6717a158
Pierre Hagelberg <pierre.hagelberg@gmail.com>
2023-03-04 20:46:16 +0100

    some changes

---
+++ lua/plugins/lsp_config.lua
@@ -0,0 +1,12 @@
+return {
+  "williamboman/mason-lspconfig",
+
+  opts = {
+    ensure_installed = {
+      "tsserver",
+      "svelte",
+      "marksman",
+      "kotlin_language_server",
+    },
+  },
+}
